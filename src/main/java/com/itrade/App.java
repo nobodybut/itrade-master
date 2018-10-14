69c3afac9c8112a4be708023980bb59ad3162421
zhangyang <zy@jindouyun.com>
2018-10-14 17:46:05 +0800

    init commit and push

---
+++ src/main/java/com/itrade/App.java
@@ -0,0 +1,13 @@
+package com.itrade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
